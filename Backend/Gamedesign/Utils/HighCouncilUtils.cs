a91f53304779c272c7f29ff9d169443775fce884
KeanuRo <91270196+KeanuRo@users.noreply.github.com>
2025-11-08 13:09:30 +0300

    additions

--- Backend/Gamedesign/Utils/HighCouncilUtils.cs
+++ Backend/Gamedesign/Utils/HighCouncilUtils.cs
@@ -1,8 +1,8 @@
 ﻿// Decompiled with JetBrains decompiler
 // Type: Backend.Gamedesign.Utils.HighCouncilUtils
 // Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 1AEE2D60-F9AC-4BB1-9F59-FC7C8812BBA8
-// Assembly location: C:\Users\Киану\RiderProjects\SampleCitK2Mod\lib\Assembly-CSharp.dll
+// MVID: 0000-0000-0000
+// Assembly location: ...\lib\Assembly-CSharp.dll
 
 using Backend.Gamedesign.DoctrinesSystem.Doctrines;
 using Backend.Gamedesign.EventSystem.Utils;
--- Frontend/Events/AnswerEntity.cs
+++ Frontend/Events/AnswerEntity.cs
@@ -1,8 +1,8 @@
 ﻿// Decompiled with JetBrains decompiler
 // Type: Frontend.Events.AnswerEntity
 // Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 1AEE2D60-F9AC-4BB1-9F59-FC7C8812BBA8
-// Assembly location: C:\Users\Киану\RiderProjects\SampleCitK2Mod\lib\Assembly-CSharp.dll
+// MVID: 0000-0000-0000
+// Assembly location: ...\lib\Assembly-CSharp.dll
 
 using Frontend.ButtonActions;
 using TMPro;
